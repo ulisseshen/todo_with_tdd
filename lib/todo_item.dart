49369b66af6b192a7c7f88536bcdb19a9d12528e
Ulisses Hen <ulisses@hens.com.br>
2023-01-05 19:19:50 -0300

    add togle button on TodoItem

--- lib/todo_item.dart
+++ lib/todo_item.dart
@@ -6,19 +6,23 @@ class TodoItem extends StatelessWidget {
   const TodoItem({
     super.key,
     required this.todo,
+    required this.onToggle,
   });
 
   final Todo todo;
+  final VoidCallback onToggle;
 
   @override
   Widget build(BuildContext context) {
     return ListTile(
+        leading: Icon(
+          Icons.check_circle,
+          color: todo.done ? Colors.green[200] : null,
+        ),
         title: Text(todo.description),
-        trailing: todo.done
-            ? Icon(
-                Icons.check_circle,
-                color: Colors.green[200],
-              )
-            : null);
+        trailing: OutlinedButton(
+          onPressed: onToggle,
+          child: Text("toggle"),
+        ));
   }
 }

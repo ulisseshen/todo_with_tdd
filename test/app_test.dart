afa6c0680fe3b8fd5435b45f92a32451d722ca81
Ulisses Hen <ulisses@hens.com.br>
2023-01-05 16:50:29 -0300

    teste para testar funcionamendo do todo list

--- test/app_test.dart
+++ test/app_test.dart
@@ -5,16 +5,34 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:todo_with_tdd/my_app.dart';
+import 'package:todo_with_tdd/todo_item.dart';
 
 void main() {
   testWidgets('Deve testar a todo list', (tester) async {
     await tester.pumpWidget(MyApp());
-    final totalFinder = find.text('Total: 3');
-    final completoFinder = find.text('Completo: 33%');
+    await tester.enterText(find.byType(TextField), 'A');
+    // Tap the add button.
+    await tester.tap(find.byType(FloatingActionButton));
+
+    // Rebuild the widget after the state has changed.
+    await tester.pump();
+
+    // Expect to find the item on screen.
+    expect(find.text('A'), findsOneWidget);
+
+    final totalFinder = find.text('Total: 1');
+    final completoFinder = find.text('Completo: 0%');
     expect(totalFinder, findsOneWidget);
     expect(completoFinder, findsOneWidget);
+
+    TodoItem item =
+        tester.widgetList<TodoItem>(find.byType(TodoItem)).elementAt(0);
+
+    expect(item.todo.description, 'A');
+    expect(item.todo.done, false);
   });
 }
